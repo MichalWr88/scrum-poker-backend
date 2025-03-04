32e6128215d86ce4403254e8c995ce482a8c115c
mmaleszewski <mmaleszewski@trans.eu>
2025-03-04 11:24:46 +0100

    feat: add server implementation with Express and Socket.IO

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import { createServer } from "node:http";
+// import { fileURLToPath } from "node:url";
+// import { dirname, join } from "node:path";
+import { Server, ServerOptions } from "socket.io";
+import { setupSocket } from "./socket";
+
+const app = express();
+const server = createServer(app);
+setupSocket(server as unknown as Partial<ServerOptions>);
+
+// const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get("/", (req, res) => {
+  res.send({ message: "Hello, world!" });
+});
+
+server.listen(3001, () => {
+  console.log("server running at http://localhost:3001");
+});
