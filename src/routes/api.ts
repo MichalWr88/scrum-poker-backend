e6f631326ff53839d1997d2f6b23fbeb733997a9
mmaleszewski <mmaleszewski@trans.eu>
2025-04-04 16:10:27 +0200

    feat: add MongoDB room and subject management routes

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,11 +1,42 @@
 import { Router } from "express";
 import { IndexController } from "../controllers";
+import { MongoRoomController } from "../mongo/room/mongoRoomController";
+import { validationAddRoom } from "../mongo/room/roomSchema";
+import { MongoSubjectController } from "../mongo/subject/subjectController";
 
-const router = Router();
+const mongoApiPath = "/api/mongo";
 const indexController = new IndexController();
+const mongoRoomController = new MongoRoomController();
+const mongoSubjectController = new MongoSubjectController();
 
 export function setRoutes(app: Router) {
-  // app.get("/api", indexController.getHome);
-  // app.post("/api/data", indexController.getData);
-  // Add more routes as needed
+  app.get("/api/status", indexController.getStatus);
+  app.get(mongoApiPath + "/rooms", mongoRoomController.getRooms);
+  app.post(
+    mongoApiPath + "/room",
+    validationAddRoom,
+    mongoRoomController.addRoom
+  );
+  app.get(mongoApiPath + "/room/:roomId", mongoRoomController.getRoom);
+  app.patch(mongoApiPath + "/room/:roomId", mongoRoomController.editRoom);
+  app.delete(mongoApiPath + "/room/:roomId", mongoRoomController.deleteRoom);
+  //
+  app.get(mongoApiPath + "/subjects", mongoSubjectController.getAllSubjects);
+  app.post(
+    mongoApiPath + "/subject/:subjectId",
+    mongoSubjectController.addSubject
+  );
+  app.get(
+    mongoApiPath + "/subject/:subjectId",
+    mongoSubjectController.getSubject
+  );
+  // votes
+  app.post(
+    mongoApiPath + "/subject/:subjectId/vote",
+    mongoSubjectController.addOrUpdateVoteToSubject
+  );
+  app.get(
+    mongoApiPath + "/user/:userId/votes",
+    mongoSubjectController.getUserVotes
+  );
 }
