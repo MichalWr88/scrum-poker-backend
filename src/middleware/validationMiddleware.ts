9bd89cffbba09a74141dd4699c5fe0a0bd62968c
mmaleszewski <mmaleszewski@trans.eu>
2025-04-04 16:09:43 +0200

    feat: add validation middleware for request body and query parameters

---
+++ src/middleware/validationMiddleware.ts
@@ -0,0 +1,47 @@
+import { Request, Response, NextFunction } from "express";
+import { z, ZodError } from "zod";
+
+import { StatusCodes } from "http-status-codes";
+
+export function validateData(schema: z.ZodObject<any, any>) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse(req.body);
+      next();
+    } catch (error) {
+      if (error instanceof ZodError) {
+        const errorMessages = error.errors.map((issue: any) => ({
+          message: `${issue.path.join(".")} is ${issue.message}`,
+        }));
+        res
+          .status(StatusCodes.BAD_REQUEST)
+          .json({ error: "Invalid data", details: errorMessages });
+      } else {
+        res
+          .status(StatusCodes.INTERNAL_SERVER_ERROR)
+          .json({ error: "Internal Server Error" });
+      }
+    }
+  };
+}
+export function validateQueryParams(schema: z.ZodObject<any, any>) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse(req.query);
+      next();
+    } catch (error) {
+      if (error instanceof ZodError) {
+        const errorMessages = error.errors.map((issue: any) => ({
+          message: `${issue.path.join(".")} is ${issue.message}`,
+        }));
+        res
+          .status(StatusCodes.BAD_REQUEST)
+          .json({ error: "Invalid query parameters", details: errorMessages });
+      } else {
+        res
+          .status(StatusCodes.INTERNAL_SERVER_ERROR)
+          .json({ error: "Internal Server Error" });
+      }
+    }
+  };
+}
